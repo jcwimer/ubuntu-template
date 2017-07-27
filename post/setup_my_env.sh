519770dc4e7fdcc5710df914dc3c02e309b4fc98
Jacob Cody Wimer <jacob.wimer@gmail.com>
2017-07-26 22:40:04 -0400

    Use interfaces.d configs

--- post/setup_my_env.sh
+++ post/setup_my_env.sh
@@ -13,11 +13,11 @@ if [ $2 == "dhcp" ]; then
   sudo cp dhcp.config /etc/network/interfaces
 else
   sudo cp static.config /etc/network/interfaces
-  sudo sed -i s/0.0.0.0/$2/g /etc/network/interfaces
+  sudo sed -i s/0.0.0.0/$2/g /etc/network/interfaces.d/eth0.cfg
 fi
 
-sudo chown root:root /etc/network/interfaces
-sudo chmod 644 /etc/network/interfaces
+sudo chown root:root /etc/network/interfaces.d -R
+sudo chmod -R 644 /etc/network/interfaces.d/*.cfg
 
 sudo bash regen-ssh.sh
 
