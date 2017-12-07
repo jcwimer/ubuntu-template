86cd074af9bf251f839cd754d2d62d86d61f9122
Jacob Cody Wimer <jacob.wimer@gmail.com>
2017-12-07 07:40:47 -0500

    Fixed swap setup

--- non-vagrant-extras.sh
+++ non-vagrant-extras.sh
@@ -4,16 +4,17 @@ NFS_SERVER='10.0.0.150'
 
 #Enable swap
 if [[ -z $(swapon -s | grep -E "^/") ]] ; then
-	fallocate -l 4G /swapfile
+	sudo fallocate -l 1G /swapfile
 	sudo touch /swapfile
 	sudo chown root:root /swapfile
 	sudo chmod 600 /swapfile
 	sudo mkswap /swapfile
 	echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 	sudo sysctl vm.swappiness=10
-	echo 'vm.swappiness=10' | sudo tee /etc/sysctl.conf
-	sudo sysctl vm.vfs_cache_pressure=50
-	echo 'vm.vfs_cache_pressure=50' | sudo tee /etc/sysctl.conf
+	echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+	#sudo sysctl vm.vfs_cache_pressure=50
+	#echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
+        sudo swapon /swapfile
 fi
 
 #Move config files
